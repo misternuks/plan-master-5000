50b85bed4614c72ad3b1b2a4555ea3486077f245
Matthew Miner <misternuks@gmail.com>
2023-06-30 15:27:49 +0900

    change configuration to connect to hub DB

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,8 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # This has been changed to 'false' for Ruby-DB-Hub
+  config.active_record.migration_error = false
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
