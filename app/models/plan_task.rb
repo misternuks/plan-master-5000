60b716883c9167066f08c745df8a1d8224872279
Matthew Miner <misternuks@gmail.com>
2023-06-30 16:32:33 +0900

    add devise plan_user, plan_task, and relevant migrations

---
+++ app/models/plan_task.rb
@@ -0,0 +1,3 @@
+class PlanTask < ApplicationRecord
+  belongs_to :plan_user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_30_043305) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_063114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "plan_tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.integer "time"
+    t.bigint "plan_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
+    t.index ["plan_user_id"], name: "index_plan_tasks_on_plan_user_id"
+  end
+
+  create_table "plan_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -22,8 +33,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_043305) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_plan_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_plan_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "plan_tasks", "plan_users"
 end
