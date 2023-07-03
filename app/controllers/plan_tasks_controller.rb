a3f503b3a07e3006ed4bcefe9ecc504ce8a2248f
Matthew Miner <misternuks@gmail.com>
2023-07-03 21:36:41 +0900

    add controller and view elements for index and change the navbar

--- app/controllers/plan_tasks_controller.rb
+++ app/controllers/plan_tasks_controller.rb
@@ -2,4 +2,14 @@ class PlanTasksController < ApplicationController
   def index
     @tasks = PlanTask.all
   end
+
+  def show
+    @task = PlanTask.find(params[:id])
+  end
+
+  private
+
+  def plan_task_params
+    params.require(:plan_task).permit(:id)
+  end
 end
