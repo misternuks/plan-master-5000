49a0d3ec23648d4a7cadb231ee67039c54b0f0a8
Matthew Miner <misternuks@gmail.com>
2023-07-04 18:09:19 +0900

    complete controller, routes, and views for plan_task

--- app/controllers/plan_tasks_controller.rb
+++ app/controllers/plan_tasks_controller.rb
@@ -7,9 +7,42 @@ class PlanTasksController < ApplicationController
     @task = PlanTask.find(params[:id])
   end
 
+  def new
+    @task = PlanTask.new
+  end
+
+  def create
+    @task = PlanTask.new(plan_task_params)
+    @task.user = current_user
+    if @task.save
+      redirect_to plan_task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = PlanTask.find(params[:id])
+  end
+
+  def update
+    @task = PlanTask.find(params[:id])
+    if @task.update(plan_task_params)
+      redirect_to plan_task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = PlanTask.find(params[:id])
+    @task.destroy
+    redirect_to plan_tasks_path, status: :see_other
+  end
+
   private
 
   def plan_task_params
-    params.require(:plan_task).permit(:id)
+    params.require(:plan_task).permit(:title, :details, :time, :completed)
   end
 end
--- app/models/plan_task.rb
+++ app/models/plan_task.rb
@@ -1,3 +1,6 @@
 class PlanTask < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :time, numericality: { in: 1..24 }
 end
